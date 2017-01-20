73b4e5fa7639aa543ae5394000d148566f7307c2
poikelp <takukelp@gmail.com>
2017-01-20 23:39:59 +0900

    コメントの追加

--- hoge/Assets/Script/LightCube.cs
+++ hoge/Assets/Script/LightCube.cs
@@ -1,13 +1,14 @@
-ï»¿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class LightCube : MonoBehaviour {
 	[SerializeField]
 	private Material cubeMat;
 
-	[SerializeField][Header("å€¤ã‚’å¢—ã‚„ã™ã»ã©ã‚†ã£ãã‚Šå¤‰åŒ–ã™ã‚‹")]
+	[SerializeField][Header("’l‚ğ‘‚â‚·‚Ù‚Ç‚ä‚Á‚­‚è•Ï‰»‚·‚é")]
 	public float changeSpeed;
 
+	
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +18,7 @@ public class LightCube : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float emissionColor = Mathf.Repeat (Time.time, 1.0f * changeSpeed);
+		//Time.time‚ğ0‚©‚çchangeSpeed‚Ì’l‚Ì’†‚ÅŒJ‚è•Ô‚µ‚Ä•Ô‚·
 		cubeMat.SetColor ("_EmissionColor", Color.HSVToRGB(emissionColor / changeSpeed, 1.0f, 0.1f));
 	}
 }
