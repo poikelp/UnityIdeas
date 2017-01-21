a1c3a8a5b2b20627f5ac42fee1c1afe3556ef161
poikelp <takukelp@gmail.com>
2017-01-22 03:52:32 +0900

    ボールがスリープ状態に入り動かなくなる問題を解決

--- 1st/Assets/blendField.cs
+++ 1st/Assets/blendField.cs
@@ -19,10 +19,19 @@ public class blendField : MonoBehaviour {
 
 		if (Mathf.Abs (hori) > 0.0f) {
 			transform.Rotate (0.0f, 0.0f, hori * Time.deltaTime * blendSpeed * -1.0f);
+			if (Mathf.Abs (transform.rotation.eulerAngles.z) > 45.0f && Mathf.Abs (transform.rotation.eulerAngles.z) < 315.0f) {
+				transform.Rotate (0.0f, 0.0f, hori * Time.deltaTime * blendSpeed);
+			}
 		}
 		if (Mathf.Abs (vert) > 0.0f) {
 			transform.Rotate (vert * Time.deltaTime * blendSpeed, 0.0f, 0.0f);
+			if (Mathf.Abs (transform.rotation.eulerAngles.x) > 45.0f && Mathf.Abs (transform.rotation.eulerAngles.x) < 315.0f) {
+				transform.Rotate (vert * Time.deltaTime * blendSpeed * -1.0f, 0.0f, 0.0f);
+			}
 		}
-		
+
+		if (Mathf.Abs (transform.rotation.eulerAngles.y) > 0.01f) {
+			transform.Rotate (0.0f, transform.rotation.eulerAngles.y * -1, 0.0f);
+		}		
 	}
 }
---
+++ 1st/Assets/cubeSup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeSup : MonoBehaviour {
+	private Rigidbody rigi;
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+		if (rigi.IsSleeping()) {
+			if (Mathf.Abs (hori) > 0.0f || Mathf.Abs (vert) > 0.0f) {
+				rigi.WakeUp();
+			}
+		}
+	}
+}
