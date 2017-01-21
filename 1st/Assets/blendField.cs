677f41ff895d110b3884c3d0d689bcc32290fc72
poikelp <takukelp@gmail.com>
2017-01-21 23:27:52 +0900

    玉を転がす

---
+++ 1st/Assets/blendField.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blendField : MonoBehaviour {
+
+	[SerializeField]
+	private float blendSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		if (Mathf.Abs (hori) > 0.0f) {
+			transform.Rotate (0.0f, 0.0f, hori * Time.deltaTime * blendSpeed * -1.0f);
+		}
+		if (Mathf.Abs (vert) > 0.0f) {
+			transform.Rotate (vert * Time.deltaTime * blendSpeed, 0.0f, 0.0f);
+		}
+		
+	}
+}
