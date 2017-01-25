33d496f91ad61507b3aa235127ea3692a2104a20
poikelp <takukelp@gmail.com>
2017-01-26 02:35:00 +0900

    玉ころがし

---
+++ 2nd/Assets/ReStand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReStand : MonoBehaviour {
+
+	private Vector3 oldPos;
+
+	// Use this for initialization
+	void Start () {
+		oldPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 diff = transform.position - oldPos;
+		if(Mathf.Abs(diff.x) > 0.0f || Mathf.Abs(diff.z) > 0.0f){
+			diff = new Vector3 (diff.x, 0.0f, diff.z);
+			transform.rotation = Quaternion.LookRotation(diff);
+		}
+
+		oldPos = transform.position;
+	}
+}
---
+++ 2nd/Assets/roll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roll : MonoBehaviour {
+
+	private Rigidbody rigi;
+
+	[SerializeField]
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		Vector3 addPow = new Vector3 (hori * Time.deltaTime * speed, 0.0f, vert * Time.deltaTime * speed);
+		rigi.AddForce (addPow);
+	}
+}
