2a84d88bbf80a407a8f47e994823a7cea941c944
poikelp <takukelp@gmail.com>
2017-01-27 01:18:50 +0900

    cameraをイイ感じに追尾させる

---
+++ 2nd/Assets/script/chaseCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaseCamera : MonoBehaviour {
+
+	private Transform tage;
+
+	public float velo = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		tage = GameObject.FindWithTag ("Player").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float newPos = Mathf.Clamp (Mathf.SmoothDamp(transform.position.x, tage.position.x, ref velo, 0.5f), 0.0f, 10.0f);
+		transform.position = new Vector3 (newPos, transform.position.y, transform.position.z);
+	}
+}
