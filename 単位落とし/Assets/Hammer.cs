e2b53872ea8733b77f520b5b25954e81402b5c07
poikelp <takukelp@gmail.com>
2017-01-30 03:30:45 +0900

    さらに単位を落としやすくなりました

--- 単位落とし/Assets/Hammer.cs
+++ 単位落とし/Assets/Hammer.cs
@@ -16,13 +16,18 @@ public class Hammer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float nextPos = Mathf.PingPong (Time.time, 4.0f);
+		float nextPos = Mathf.PingPong (Time.time, 4.0f) - 0.5f;
 		transform.position = new Vector3 (-1.5f, nextPos, -1.5f);
 
-		if (Input.GetButton ("Fire1")) {
+		if (Input.GetButtonDown ("Fire1") && !swinging) {
 			swinging = true;
 //			transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0.0f, 30.0f, 0.0f), Time.deltaTime);
-			rigi.AddTorque(0.0f, torque * Time.deltaTime, 0.0f);
+			rigi.AddTorque(0.0f, torque, 0.0f);
+		}
+		if (transform.eulerAngles.y > 50.0f) {
+			rigi.angularVelocity = Vector3.zero;
+			transform.eulerAngles = Vector3.zero;
+			swinging = false;
 		}
 	}
 		
