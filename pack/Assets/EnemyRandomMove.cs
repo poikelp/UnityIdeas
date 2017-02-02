af7ed62bfcad750f2382b7dae87ce243ff5a2ba9
poikelp <user@USER-no-MacBook-Air.local>
2017-02-02 16:24:06 +0900

    kuso

--- pack/Assets/EnemyRandomMove.cs
+++ pack/Assets/EnemyRandomMove.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyRandomMove : MonoBehaviour {
-
+	private CharacterController eCon;
+	private GameObject player;
 	// Use this for initialization
 	void Start () {
-		
+		eCon = GetComponent<CharacterController> ();
+		player = GameObject.FindWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 pPos = player.transform.position;
+		transform.LookAt (pPos);
+		eCon.Move (transform.forward * Time.deltaTime * 5.0f);
 	}
 }
