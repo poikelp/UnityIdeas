d4b078d5d890eee401b5132dfe7e2d5d2b6cac2d
poikelp <takukelp@gmail.com>
2017-02-13 23:20:26 +0900

    rangeSet

--- tapMoveTest/Assets/Move.cs
+++ tapMoveTest/Assets/Move.cs
@@ -7,16 +7,43 @@ public class Move : MonoBehaviour {
 	private RaycastHit hit;
 	private Ray ray;
 
+	[SerializeField]
+	private float defRange;
+	private Transform movRanTra;
+	private float moveRange;
+
 	void Start () {
 		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		movRanTra = GameObject.Find ("moveRange").transform;
+		moveRange = defRange;
+		SetRangeSprite ();
 	}
 
 	void Update () {
 		if (Input.GetMouseButtonDown(0)){
 			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, out hit, 100f)){
-				agent.SetDestination(hit.point);
+				if (!hit.collider.gameObject.Equals(this.gameObject)) {
+					float dist = Vector3.Distance (transform.position, hit.point);
+					if (dist < moveRange) {
+						agent.SetDestination (hit.point);
+						moveRange -= dist;
+						SetRangeSprite ();
+					}
+				}
 			}       
 		}
+		if (Input.GetMouseButtonDown (1)) {
+			ResetRange ();
+		}
+	}
+
+	void SetRangeSprite () {
+		movRanTra.localScale = new Vector3 (moveRange, 0.00001f, moveRange);
+	}
+
+	void ResetRange () {
+		moveRange = defRange;
+		SetRangeSprite ();
 	}
 }
