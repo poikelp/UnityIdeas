ef0fe9f62502a579e1fd12a72fa9bb7464e45ac0
poikelp <takukelp@gmail.com>
2017-02-15 05:01:53 +0900

    System

---
+++ tapMoveTest/Assets/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public float HP;
+	public float pow;
+	public float def;
+	public float mov;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
