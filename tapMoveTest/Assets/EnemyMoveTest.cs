5b38049429fa192fc26e1a6716fb9f28330c6674
poikelp <takukelp@gmail.com>
2017-02-16 02:59:23 +0900

    enemy move 1second every turn

---
+++ tapMoveTest/Assets/EnemyMoveTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveTest : MonoBehaviour {
+
+	private UnityEngine.AI.NavMeshAgent agent;
+
+	[SerializeField]
+	private GameManager manager; 
+
+	[SerializeField]
+	private Transform target;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetTarget () {
+		if (!manager.turn) {
+			agent.SetDestination (target.position);
+			Invoke ("StopMove", 1.0f);
+		}
+	}
+
+	void StopMove () {
+		agent.ResetPath ();
+	}
+}
