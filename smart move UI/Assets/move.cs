4a4994873982f140039472061f3122d0446495f5
poikelp <takukelp@gmail.com>
2017-04-23 23:12:04 +0900

    ma

---
+++ smart move UI/Assets/move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	private Vector2 oldTapPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		if (Input.GetMouseButtonDown (0)) {
+			
+		}
+
+	}
+}
