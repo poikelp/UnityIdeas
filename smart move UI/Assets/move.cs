66f6ed4e048b5ba887dc6663d75fc47b6a7fb093
poikelp <takukelp@gmail.com>
2017-04-24 00:06:28 +0900

    horizontal move

--- smart move UI/Assets/move.cs
+++ smart move UI/Assets/move.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class move : MonoBehaviour {
 
-	private Vector2 oldTapPos;
+	private Vector2 oldTapPos/* = new Vector2(0.0f, 0.0f)*/;
+	[SerializeField]
+	private float speed = 5.0f;
+	private float oldHori = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +19,25 @@ public class move : MonoBehaviour {
 		float hori = Input.GetAxis ("Horizontal");
 		float vert = Input.GetAxis ("Vertical");
 
+		//クリックしたときの座標を取得
 		if (Input.GetMouseButtonDown (0)) {
-			
+			oldTapPos = Input.mousePosition;
 		}
 
+		if (Input.GetMouseButton (0)) {
+			if (oldTapPos.x > Input.mousePosition.x) {
+				hori = -1.0f;
+				oldHori = hori;
+			} else if (oldTapPos.x < Input.mousePosition.x) {
+				hori = 1.0f;
+				oldHori = hori;
+			} else if (oldTapPos.x == Input.mousePosition.x) {
+				hori = oldHori;
+			}
+			oldTapPos = Input.mousePosition;
+		}
+
+		Vector3 velo = new Vector3 (hori * speed * Time.deltaTime, 0.0f, vert * speed * Time.deltaTime);
+		transform.position += velo;
 	}
 }
