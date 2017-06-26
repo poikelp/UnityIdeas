e45fee1dddaa688dd3a8a71e6770c8310764a6d9
poikelp <takukelp@gmail.com>
2017-06-27 00:07:22 +0900

    cardboard

---
+++ WebCamTest/Assets/turnCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnCube : MonoBehaviour {
+	Rigidbody rigi;
+	[SerializeField]
+	Vector3 pow;
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigi.AddTorque (pow);
+	}
+}
